85c7002c4b794d949467ed0fa7a1f3d054b56a9f
softtechie007 <iqbalhamsa01@gmail.com>
2024-10-05 12:13:27 +0530

    New angular project binding is created and property binding is done

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+  public name = "Iqbal";
+  public siteUrl: string | undefined;
+
+  constructor(){
+    if (typeof window !== 'undefined') {
+      this.siteUrl = window.location.href;
+    } else {
+      this.siteUrl = 'Site URL not available in this environment';
+    }
+  }
+
+  greetUser(){
+    return "Hello " + this.name;
+  }
+}
